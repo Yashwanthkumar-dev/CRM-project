4aecfc48a9fccb88e5f9e406d2f150b324f1bfc6
Yashwanth kumar .N <yr375003@gail.com>
2026-03-20 19:13:21 +0530

    fea: created a column in leadEntity

--- src/main/java/com/example/crm/Model/LeadEntity.java
+++ src/main/java/com/example/crm/Model/LeadEntity.java
@@ -25,6 +25,7 @@ public class LeadEntity {
     private String phone;
     private String location;
     private String company;
+    private String source;
     @CreationTimestamp
     @Column(updatable = false , nullable = false)
     private LocalDateTime createdAt;
